aad1c29c2487c4deb20d3bb85c636dc773e26803
ichenq <ichenq@gmail.com>
2015-04-28 17:52:11 +0800

    Update premake script

--- premake5.lua
+++ premake5.lua
@@ -4,107 +4,84 @@
 
 solution 'luazmq'
     configurations {'Debug', 'Release'}
-    --flags {'ExtraWarnings'}
     targetdir 'bin'
-    platforms {'x32', 'x64'}
 
-    configuration 'Debug'
-        defines { 'DEBUG' }
-        flags { 'Symbols' }
+    filter 'configurations:Debug'
+        defines     'DEBUG'
+        flags       'Symbols'
 
-    configuration 'Release'
-        defines { 'NDEBUG' }
-        flags { 'Symbols', 'Optimize' }
+    filter 'configurations:Release'
+        defines     'NDEBUG'
+        flags       'Symbols'
+        optimize    'On'
 
-    configuration 'vs*'
+     filter 'action:vs*'
         defines
         {
             'WIN32',
             '_WIN32_WINNT=0x0600',
             '_CRT_SECURE_NO_WARNINGS',
-            '_SCL_SECURE_NO_WARNINGS',
             'NOMINMAX',
             'snprintf=_snprintf',
             'inline=__inline',
-            'LUA_BUILD_AS_DLL',
         }
 
-    configuration 'gmake'
-        buildoptions '-rdynamic'
-        defines
-        {
-            '__STDC_LIMIT_MACROS',
-        }
-        links
-        {
-            'm',
-            'rt',
-            'dl',
-        }
-
-    if os.get() == 'windows' then    
+    filter 'action:gmake'
+        buildoptions    '-rdynamic'
+        defines         '__STDC_LIMIT_MACROS'
+  
+    filter 'system:windows'
+        defines 'LUA_BUILD_AS_DLL'
+            
+    filter 'system:linux'
+        defines 'LUA_USE_LINUX'
+            
 	project 'lua'
-		language 'C'
-		kind 'ConsoleApp'
-		location 'build'
-		files
-		{
-			'deps/lua/src/lua.c',
-		}
-		includedirs
-		{
-			'deps/lua/src',
-		}
-		libdirs 'bin'
-		links 'lua5.3'
+        targetname  'lua'
+		language    'C'
+		kind        'ConsoleApp'
+		location    'build'
+        
+        includedirs 'deps/lua/src'
+		files       'deps/lua/src/lua.c'
+		libdirs     'bin'
+		links       'lua5.3'
+        
+        filter 'system:linux'
+            links   {'dl', 'readline'}
 		
 	project 'lua5.3'
-		language 'C'
-		kind 'SharedLib'
-		location 'build' 		
+        targetname  'lua5.3'
+		language    'C'
+		kind        'SharedLib'
+		location    'build' 		
+        
+        includedirs 'deps/lua/src'
 		files
 		{
 			'deps/lua/src/*.h',
 			'deps/lua/src/*.c',
 		}
-		excludes
+		removefiles
 		{
 			'deps/lua/src/lua.c',
 			'deps/lua/src/luac.c',
-		}
-		includedirs
-		{
-			'deps/lua/src',
-		}		
-	end        
+		}      
         
     project 'luazmq'
-        language 'C'
-        kind 'SharedLib'
-        location 'build'
-        uuid '86C52891-A665-47BF-9107-8EA58606C41A'
-        defines 'LUA_LIB'
-        files
-        {
-            'src/*.c',
-        }
-        includedirs
-        {
-            'deps/lua/src',
-        }
+        targetname  'luazmq'
+        language    'C'
+        kind        'SharedLib'
+        location    'build'
         
-        links 'lua5.3'
-        if os.get() == 'linux' then
-        links 
-        {
-            'zmq',
-            'sodium',
-        }
-        else
-        links 
-        {
-            'libzmq',
-            'libsodium',
-        }            
-        end
+        defines     'LUA_LIB'
+        includedirs 'deps/lua/src'
+        files       'src/*.c'
+        links       'lua5.3'
+        
+        filter 'system:linux'
+            links  {'zmq', 'sodium'}
+            
+        filter 'system:windows'
+            links  {'libzmq', 'libsodium'}
         
