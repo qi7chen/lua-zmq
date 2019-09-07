853d1ac167f8acdedf1d0752811d3e586846f30f
johnnie.chen <ichenq@outlook.com>
2019-09-07 11:15:10 +0400

    Fix tab space

--- premake5.lua
+++ premake5.lua
@@ -8,11 +8,11 @@ solution 'luazmq'
 
     filter 'configurations:Debug'
         defines     'DEBUG'
-        symbols 	'On'
+        symbols     'On'
 
     filter 'configurations:Release'
         defines     'NDEBUG'
-		symbols 	'On'
+        symbols     'On'
         optimize    'On'
 
      filter 'action:vs*'
@@ -28,47 +28,47 @@ solution 'luazmq'
 
     filter 'system:windows'
         defines 'LUA_BUILD_AS_DLL'
-            
+
     filter 'system:linux'
         defines 'LUA_USE_LINUX'
-            
-	project 'lua5.3'
+
+    project 'lua5.3'
         targetname  'lua5.3'
-		language    'C'
-		kind        'SharedLib'
-		location    'build' 		
-        
+        language    'C'
+        kind        'SharedLib'
+        location    'build'
+
         includedirs 'deps/lua/src'
-		files
-		{
-			'deps/lua/src/*.h',
-			'deps/lua/src/*.c',
-		}
-		removefiles
-		{
-			'deps/lua/src/lua.c',
-			'deps/lua/src/luac.c',
-		}      
-        
+        files
+        {
+            'deps/lua/src/*.h',
+            'deps/lua/src/*.c',
+        }
+        removefiles
+        {
+            'deps/lua/src/lua.c',
+            'deps/lua/src/luac.c',
+        }
+
     project 'luazmq'
         targetname  'luazmq'
         language    'C'
         kind        'SharedLib'
         location    'build'
-        
+
         defines     'LUA_LIB'
-        includedirs 
+        includedirs
         {
             'deps/lua/src',
             'deps/zmq/include'
         }
         files       'src/*.c'
         links       'lua5.3'
-        
+
         filter 'system:linux'
             links  {'m', 'zmq', 'sodium'}
-            
+
         filter 'system:windows'
             libdirs 'deps/zmq/lib'
             links  {'libzmq'}
-        
+
